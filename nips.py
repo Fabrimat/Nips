b3344c8465efcc33bce226d3efdaa33ab7e99712
Fabrimat <lr.fabrizio@gmail.com>
2015-08-31 18:50:04 +0200

    Added logging

--- nips.py
+++ nips.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 import os
 import time
 from subprocess import Popen
@@ -6,76 +6,73 @@ import socket
 import subprocess
 import sys
 from datetime import datetime
-#import logging
-#import functionscanner
+import logging
+
+#Define variables
+iprange=None
+showerr=None
+p = [] # ip -> process
+act=0
+nrp=0
+err=0
+opn=0
+clsd=0
+port=0
+t1=0
+t2=0
+total=0
+devnull = open(os.devnull, 'wb')
 
 # Clear the screen
 subprocess.call('clear', shell=True)
 
-# Console colors
-W = '\033[0m'    # white (normal)
-R = '\033[31m'   # red
-G = '\033[32m'   # green
-O = '\033[33m'   # orange
-B = '\033[34m'   # blue
-P = '\033[35m'   # purple
-C = '\033[36m'   # cyan
-GR = '\033[37m'  # gray
-T = '\033[93m'   # tan
+#Configuring logging
+ogger = logging.getLogger('LogScanner')
+logging.basicConfig(filename='Nips.log',format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.DEBUG)
 
+#Presentation
+print ("Nips v0.1 - Network Ip Port Scanner")
+print ("... type enter to continue ")
+raw_input()
 
+logging.info('######### NEW LOG SESSION ########')
 
-#def ip_scanner():
+#Ask for inputs
+iprange=raw_input("Enter the ip range ( ex: 192.168.0 ) - ")
+logging.info('IP Range: %s',iprange)
+port=input("Enter the port ( ex: 80 ) - ")
+logging.info('Port: %s',port)
+showerr=raw_input("Print only the active ips? (y/n) - ")
+logging.info('Only active ips? %s',showerr)
+
+#A little alert
+print("")
+print 'The outputs will be saved as "Nips.log".'
+time.sleep(0.5)
+print ("")
+
+#Checking errors
+if iprange == "" or type(port) != int or port == 0 or len(iprange) > 11 or iprange.count(".") != 2:
+    print ("-=" * 21)
+    print ("Please check your inputs and try again...")
+    print ("-=" * 21)
+    logging.error('Invalid Inputs: %s - %s - %s - %s - %s',iprange,type(port),port,len(iprange),iprange.count("."))
+    sys.exit()
+
 try:
-    #Presentation
-    print ("Nips v0.1 - Network Ip Port Scanner")
-    print ("... type enter to continue ")
-    raw_input()
-
-    #Ask for inputs and define variables
-    str1=raw_input("Enter the ip range ( ex: 192.168.0 ) - ")
-    port=80
-    port=input("Enter the port ( default: 80 ) - ")
-    showerr=raw_input("Print only the active ips? (y/n) - ")
-    devnull = open(os.devnull, 'wb')
-    #outlog=raw_input("Save the outputs into a log file? (y/n) - ")
-
-    #starttime = datetime.now()
-
-    #if outlog == "y":
-    #    logfilename=raw_input("Type the name of the log file ( ex: scanlog ) - ")
-    #    logging.basicConfig(level=logging.DEBUG, filename=(logfilename,starttime), filemode="a+",
-    #                    format="%(asctime)-15s %(levelname)-8s %(message)s")
-
-    #print ("Scanning ip range ",str1)
-
-    if str1 == "" or type(port) != int:
-     print ("-=" * 21)
-     print ("Please check your inputs and try again...")
-     print ("-=" * 21)
-     sys.exit()
-
-     #Information variables
-    p = [] # ip -> process
-    act = 0
-    nrp = 0
-    err = 0
-    opn = 0
-    clsd = 0
-
     # Check what time the scan started
     t1 = datetime.now()
-
-    print ("")
-    print (">Please wait, pinging remote ips.")
-    print ("")
     #Start pinging and scanning
+    print ">Scanning ip range ",iprange
+    logging.info('Scanning ip range')
+    print ("")
     for n in range(1,255): # start ping processes
-        ip = str1+".%d" % n
+        ip = iprange+".%d" % n
         p.append((ip, Popen(['ping', '-c', '3', ip], stdout=devnull)))
 
     #print ("=-" * 30)
-    print (">Starting port scan.")
+    print (">Starting port scan. This can be take a while.")
+    logging.info('Starting port scan.')
     #print ("=-" * 30)
     print ("")
 
@@ -85,23 +82,28 @@ try:
                 p.remove((ip, proc)) # this makes it O(n**2)
                 if proc.returncode == 0:
                     print('%s active' % ip)
+                    logging.info('%s active',ip)
                     act = act + 1
                     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                     result = sock.connect_ex((ip, port))
                     sock.close()
                     if result == 0:
                         print ("\tPort {}: Open".format(port))
+                        logging.info('Port %s: Open',port)
                         opn = opn + 1
                     else:
                         print ("\tPort {}: Closed".format(port))
+                        logging.info('Port %s: Closed',port)
                         clsd = clsd + 1
                 elif proc.returncode == 2:
                     if showerr == "n":
                         print('%s no response' % ip)
+                    logging.info('%s no response',ip)
                     nrp=nrp+1
                 else:
                     if showerr == "n":
                         print('%s error' % ip)
+                    logging.info('%s error',ip)
                     err=err+1
         time.sleep(.04)
     devnull.close()
@@ -110,12 +112,15 @@ try:
 #Errors outputs
 except KeyboardInterrupt:
     sys.exit("\n You pressed Ctrl+C")
+    logging.info('Ctrl+C were pressed')
 
 except socket.gaierror:
     sys.exit('\n Hostname could not be resolved. Exiting')
+    logging.info('Hostname could not be resolved.')
 
 except socket.error:
     sys.exit("\n Couldn't connect to server")
+    logging.info("Couldn't connect to server")
 
 # Checking the time again
 t2 = datetime.now()
@@ -123,16 +128,24 @@ t2 = datetime.now()
 # Calculates the difference of time, to see how long it took to run the script
 total =  t2 - t1
 
+#Logging final information
+logging.info('Scanning Completed in: %s',total)
+logging.info('Active ips: %s',act)
+logging.info('Error ips: %s',err)
+logging.info('No reponse ips: %s',nrp)
+logging.info('Open ports: %s',opn)
+logging.info('Closed ports: %s',clsd)
+
 # Printing the information to screen
-print ('Scanning Completed in: ', total)
-print ("")
-print ("-=" * 9)
-print ("Network status")
-print ("-=" * 9)
-print ("Active ips [ ",act," ]")
-print ("Error ips [ ",err," ]")
-print ("No response [ ",nrp," ]")
-print ("Open ports   [ ",opn," ]")
-print ("Closed ports [ ",clsd," ]")
-print ("")
-print ("Good bye!")
+print 'Scanning Completed in: ', total
+print ""
+print "-=" * 9
+print "Network status"
+print "-=" * 9
+print "Active ips [ ",act," ]"
+print "Error ips [ ",err," ]"
+print "No response ips [ ",nrp," ]"
+print "Open ports   [ ",opn," ]"
+print "Closed ports [ ",clsd," ]"
+print ""
+print "Good bye!"
