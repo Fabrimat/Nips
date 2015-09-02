2d03915ff75764a6fd6ddc1a6c6aae113da15982
Fabrizio La Rosa <lr.fabrizio@gmail.com>
2015-09-02 17:45:36 +0200

    Added colors

--- Nips.py
+++ Nips.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python2
+'''
+    Nips v1.1 - Portsweep
+    Author: Fabrimat
+    Repository: https://github.com/Fabrimat/Nips
+'''
 import os
 import time
 from subprocess import Popen
@@ -8,12 +13,27 @@ import sys
 from datetime import datetime
 import logging
 
+# Console colors
+W = '\033[0m'    # (normal)
+R = '\033[31m'   # red
+G = '\033[32m'   # green
+O = '\033[33m'   # orange
+B = '\033[34m'   # blue
+P = '\033[35m'   # purple
+C = '\033[36m'   # cyan
+GR = '\033[37m'  # gray
+T = '\033[93m'   # tan
+
+#Define global variables
+programinfo = "Nips v1.1 - Portsweep by Fabrimat"
+
 # Clear the screen
 def cls():
     if os.name == "posix":
-        subprocess.call('clear', shell=True)
+        os.system('clear')
     elif os.name == "nt":
         os.system(['clear','cls'][os.name == 'nt'])
+
 
 cls()
 
@@ -40,9 +60,8 @@ def ascipres():
     print "\t                               p:::::::p                            "
     print "\t                               p:::::::p                            "
     print "\t                               p:::::::p                            "
-    print "\t                               ppppppppp                            "
+    print "\t                               ppppppppp                        v1.1"
     print "\n\n\n\n"
-    return
 
 ascipres()
 
@@ -57,22 +76,10 @@ opn=0
 clsd=0
 port=0
 devnull = open(os.devnull, 'wb')
-programinfo = "Nips v1.0 - Portsweep by Fabrimat"
-
-# Console colors
-W = '\033[0m'    # (normal)
-R = '\033[31m'   # red
-G = '\033[32m'   # green
-O = '\033[33m'   # orange
-B = '\033[34m'   # blue
-P = '\033[35m'   # purple
-C = '\033[36m'   # cyan
-GR = '\033[37m'  # gray
-T = '\033[93m'   # tan
 
 #Configuring logging
 logger = logging.getLogger('LogScanner')
-logging.basicConfig(filename='Nips.log',format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.DEBUG)
+logging.basicConfig(filename='nips.log',format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.DEBUG)
 
 #Presentation
 print R + programinfo + W
@@ -88,42 +95,58 @@ logging.info('%s',programinfo)
 #Ask for inputs
 print R + programinfo + W
 print T + "Please enter the inputs.\n" + W
-iprange=raw_input("Enter the ip range " + GR + "( ex: 192.168.0 )" + W + " - ")
+iprange=raw_input( G + "Enter the ip range " + GR + "( ex: 192.168.0 )" + W + " - ")
 logging.info('IP Range: %s',iprange)
-port=input("Enter the port " + GR + "( ex: 80 )" + W + " - ")
+port=input( G + "Enter the port " + GR + "( ex: 80 )" + W + " - ")
 logging.info('Port: %s',port)
-showerr=raw_input("Print only the active ips? "+ GR + "(y/n)" + W + " - ")
+showerr=raw_input( G + "Print only the active ips? "+ GR + "(y/n)" + W + " - ")
 logging.info('Only active ips? %s',showerr)
-showhost=raw_input("Show the hostnames? " + GR + "(y/n)" + W + " - ")
+showhost=raw_input( G + "Show the hostnames? " + GR + "(y/n)" + W + " - ")
 logging.info('Hostnames? %s',showhost)
 #socketdeftime=input("Enter the timeout " + GR + "( ex: 0.5 )" + W + " - ")
 
 #A little alert
 print("")
-print 'The outputs will be saved as ' + GR + '"Nips.log"' + W + '.'
+print  G + 'The outputs will be saved as ' + GR + '"nips.log"' + G + '.' + W
 time.sleep(0.5)
 print ("")
 
 #Checking errors
-if iprange == "" or type(port) != int or port == 0 or len(iprange) > 11 or len(iprange) < 5 or iprange.count(".") != 2 or (showerr != "y" and showerr != "n") or(showhost != "y" and showhost != "n"):
-    print "-=" * 21
-    print R + "Please check your inputs and try again..." + W
-    print "-=" * 21
-    logging.error('Invalid Inputs: %s - %s - %s - %s - %s',iprange,type(port),port,len(iprange),iprange.count("."))
-    sys.exit()
+error = 0
+if iprange == "" or len(iprange) > 11 or len(iprange) < 5 or iprange.count(".") != 2:
+    print G + "-=" * 29
+    print R + "Invalid Ip, please check your inputs and try again..." + W
+    print G + "-=" * 29
+    logging.error('Invalid Ip: %s. Please use the following template: 192.168.0',iprange)
+    error = 1
+if type(port) != int or port < 1 or port > 65535:
+    print G + "-=" * 29
+    print R + "Invalid Port, please check your inputs and try again..." + W
+    print G + "-=" * 29
+    logging.error('Invalid Port: %s. Enter a valid value (1 - 65535)',port)
+    error = 1
+if (showerr != "y" and showerr != "n" and showerr != "yes" and showerr != "no") or(showhost != "y" and showhost != "n" and showhost != "yes" and showhost != "no"):
+    print G + "-=" * 29
+    print R + "Invalid Inputs, please check your inputs and try again..." + W
+    print G + "-=" * 29
+    logging.error('Invalid Inputs: %s - %s. Enter a valid value (y/n)',showerr,showhost)
+    error = 1
+if error == 1:
+    print ""
+    sys.exit('Check nips.log for details.\n')
 
 try:
     # Check what time the scan started
     t1 = datetime.now()
     #Start pinging and scanning
-    print GR + "Scanning ip range " + C,iprange + ".1 - " + iprange + ".255" + W
+    print GR + "Scanning ip range " + C,iprange + ".1 - " + iprange + ".255" + R
     logging.info('Scanning ip range')
     print ("")
     for n in range(1,255): # start ping processes
         ip = iprange+".%d" % n
         p.append((ip, Popen(['ping', '-c', '3', ip], stdout=devnull)))
 
-    print ("Starting port scan. This can be take a while.")
+    print GR + "Starting port scan. This can be take a while." + R
     logging.info('Starting port scan.')
     print ("")
 
@@ -132,24 +155,24 @@ try:
             if proc.poll() is not None: # ping finished
                 p.remove((ip, proc)) # this makes it O(n**2)
                 if proc.returncode == 0:
-                    print('%s active' % ip)
+                    print T + ip + GR + ':' + G + ' active' + W
                     logging.info('%s active',ip)
                     act = act + 1
                     socket.setdefaulttimeout(0.5)
                     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                     result = sock.connect_ex((ip, port))
                     if result == 0:
-                        print "\tPort" ,port, ":" + G + " Open" + W
+                        print GR + "\tPort" + T ,port, GR + ":" + G + " Open" + W
                         logging.info('Port %s: Open',port)
                         opn = opn + 1
                     else:
-                        print "\tPort" ,port, ":" + R + " Closed" + W
+                        print GR + "\tPort" + T ,port, GR + ":" + R + " Closed" + W
                         logging.info('Port %s: Closed',port)
                         clsd = clsd + 1
-                    if showhost == "y":
+                    if showhost == "y" or showhost == "yes":
                         try:
                             host = socket.gethostbyaddr(ip)
-                            print "\tHostname:",host[0]
+                            print GR + "\tHostname:"+ G,host[0], W
                             logging.info("Hostname: %s",host[0])
                             logging.info("Alias list: %s",host[1])
                             logging.info("Ip Addresses List: %s",host[2])
@@ -159,14 +182,14 @@ try:
                     sock.close()
 
                 elif proc.returncode == 2:
-                    if showerr == "n":
-                        print('%s no response' % ip)
-                    logging.info('%s no response',ip)
+                    if showerr == "n" or showerr == "no":
+                        print T + ip + GR + ':' + R + ' no response' + W
+                        logging.info('%s no response',ip)
                     nrp=nrp+1
                 else:
-                    if showerr == "n":
-                        print('%s error' % ip)
-                    logging.info('%s error',ip)
+                    if showerr == "n" or showerr == "no":
+                        print T + ip + GR + ':' + R + ' error' + W
+                        logging.info('%s error',ip)
                     err=err+1
         time.sleep(.04)
     devnull.close()
@@ -176,14 +199,6 @@ try:
 except KeyboardInterrupt:
     sys.exit("\n You pressed Ctrl+C")
     logging.info('You pressed Ctrl+C')
-
-#except socket.gaierror:
-#    sys.exit('\n Hostname could not be resolved. Exiting')
-#    logging.info('Hostname could not be resolved.')
-
-#except socket.error:
-#    sys.exit("\n Couldn't connect to server")
-#    logging.info("Couldn't connect to server")
 
 # Checking the time again
 t2 = datetime.now()
@@ -200,15 +215,16 @@ logging.info('Open ports: %s',opn)
 logging.info('Closed ports: %s',clsd)
 
 # Printing the information to screen
-print 'Scanning Completed in: ', total
 print ""
-print "-=" * 9
-print "Network status"
-print "-=" * 9
-print "Active ips \t[ " + C,act, W + " ]"
-print "Error ips \t[ " + C,err, W + " ]"
-print "No response ips [ " + C,nrp, W + " ]"
-print "Open ports   \t[ " + C,opn, W + " ]"
-print "Closed ports \t[ " + C,clsd, W + " ]"
+print GR + 'Scanning Completed in: '+ C, total, W
 print ""
-print "Good bye!"
+print GR + "-=" * 15 + W
+print R + "\tNetwork status" + W
+print GR + "-=" * 15 + W
+print G + "Active ips \t" + GR + "[ " + C,act, GR + " ]" + W
+print R + "Error ips \t" + GR + "[ " + C,err, GR + " ]" + W
+print R + "No response ips " + GR + "[ " + C,nrp, GR + " ]" + W
+print G + "Open ports   \t" + GR + "[ " + C,opn, GR + " ]" + W
+print R + "Closed ports \t" + GR + "[ " + C,clsd, GR + " ]" + W
+print ""
+print P + "Good bye!" + W
